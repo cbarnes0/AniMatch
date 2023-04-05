c8daa708cc1bf950ccab2bea583d6988b36cca41
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-04-05 10:26:26 -0400

    added random api fetch js file

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//const sequelize = require('./config/connection');
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const getRandomAnime = require('./public/js/animeFetch')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// here for testing
+getRandomAnime()
 
-app.use(session(sess));
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 300000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session(sess));
 
-app.use(routes);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
