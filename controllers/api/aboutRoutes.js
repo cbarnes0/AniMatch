069b5366c3dec83e57bc7a1602441bcd13e206ba
cbarnes0 <carsonbarnes98@gmail.com>
2023-04-12 20:15:05 -0400

    about route added

---
+++ controllers/api/aboutRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const path = require('path');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/about.html'));
+  });
+
+module.exports = router;
